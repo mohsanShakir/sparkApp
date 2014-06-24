e52810ffbd34c922f9cbcb3d5d2364d3a9f78243
Mohsan Shakir <mohsan.shakir@formpipe.com>
2014-06-24 14:29:37 +0200

    added http

--- src/main/resources/public/js/controllers/controller.js
+++ src/main/resources/public/js/controllers/controller.js
@@ -7,7 +7,19 @@ sparkappControllers.controller('homeController', ['$scope',
         $scope.besked = "HOME";
     }]);
 
-sparkappControllers.controller('projectController', ['$scope',
-    function($scope) {
+sparkappControllers.controller('projectController', ['$scope','$http',
+    function($scope,$http) {
+
         $scope.besked = "PROJECT";
+        $http({method: 'GET', url: '/hello'}).
+            success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $scope.response = data;
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+
     }]);
